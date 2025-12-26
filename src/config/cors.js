effc78fce260597f8b7c4d4faab80397cdc99f6d
hannan-siddiqui <hannansiddiqui774@gmail.com>
2025-12-26 21:46:40 +0530

    cors issue fix

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,9 +1,11 @@
 /**
  * CORS Configuration
  */
-const allowedOrigins = process.env.ALLOWED_ORIGINS
-    ? process.env.ALLOWED_ORIGINS.split(',')
-    : ['http://localhost:5174', 'http://localhost:5173', 'http://localhost:3000'];
+// const allowedOrigins = process.env.ALLOWED_ORIGINS
+//     ? process.env.ALLOWED_ORIGINS.split(',')
+//     : ['http://localhost:5174', 'http://localhost:5173', 'http://localhost:3000'];
+
+const allowedOrigins = "https://sauronfrontend.vercel.app/"
 
 const corsOptions = {
     origin: function (origin, callback) {
